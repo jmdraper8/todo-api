8549db322e9c69e7354429d78ad495a61c89e8f8
John Draper <johndraper@icloud.com>
2016-06-14 18:22:08 +1000

    todo POST Added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 //provided by heroku process.env.PORT
 var PORT = process.env.PORT || 3000;
 var todos = [];
-
 var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
@@ -39,6 +41,31 @@ app.get('/todos/:id', function (req, res) {
 	res.send('Asking for todo with id of ' + req.params.id);
 
 
+});
+
+//POST /todos/
+app.post('/todos', function (req, res) {
+	// var body = req.body;
+	// var todo = {
+	// 	id: 0,
+	// 	description: '',
+	// 	completed: false
+	// }
+
+	// console.log(todos.length);
+	// todo.id = todos.length + 1;
+	// todo.description = body.description;
+	// todos.push(todo);
+	// console.log(todos);
+	// console.log('Decription: ' + body.description);
+
+	var body = req.body;
+
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(todos);
 });
 
 
