434bdb4f8351b2954fc7259871531d7cbd2b2434
John Draper <johndraper@icloud.com>
2016-06-19 23:33:03 +1000

    Convert GET to use sequelize

--- server.js
+++ server.js
@@ -46,15 +46,26 @@ app.get('/todos', function(req, res) {
 //GET Request /todos/:id 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+
+	// if (matchedTodo) {
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).send();
+	// }
+
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());			
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(400).json(e);
 	});
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
 
 });
 
