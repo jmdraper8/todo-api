6ff4b6e2f290ff4ed3ad25eaf66565454dfe2749
John Draper <johndraper@icloud.com>
2016-06-20 13:05:48 +1000

    Udpated Delete with DB.Calls to the API

--- server.js
+++ server.js
@@ -77,18 +77,37 @@ app.post('/todos', function (req, res) {
 app.delete('/todos/:id', function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			//db.todo.destroy(todo);
+			db.todo.destroy({
+				where: {
+					id: todoId
+				}
+			});
+			res.json(todo.toJSON());				
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(400).json(e);
 	});
 
-	if (!matchedTodo) {
-		res.status(404).json({
-			"Error": "No todo found with id: " + todoId
-		});
-	} else {
-		todos = _.without(todos, matchedTodo)
-		res.json(matchedTodo);
-	}
+
+
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+
+	// if (!matchedTodo) {
+	// 	res.status(404).json({
+	// 		"Error": "No todo found with id: " + todoId
+	// 	});
+	// } else {
+	// 	todos = _.without(todos, matchedTodo)
+	// 	res.json(matchedTodo);
+	// }
 
 });
 
